1e3d18b3bd9f9bde05cfac59af7c4956ec8a2a1c
Erika Vidal <erikadianavidalcapillo@gmail.com>
2017-07-14 00:19:05 -0500

    descarga cv

--- public/js/main.js
+++ public/js/main.js
@@ -12,4 +12,22 @@ $(_=>{
     $('.scrollspy').scrollSpy();
 
     $(".button-collapse").sideNav();
+
+    $('.descarga').on('click',_=>{
+        document.location="https://drive.google.com/file/d/0B8DCWyMbksMoMEwwU1FSTDRaTkxpWHNkWW90S1NjLWVGSVcw/view?usp=sharing";
+    })
+
+    $('.btn-up').click(function(){
+        $('body, html').animate({
+            scrollTop: '0px'
+        }, 300);
+    });
+
+    $(window).scroll(function(){
+        if( $(this).scrollTop() > 0 ){
+            $('.btn-up').slideDown(300);
+        } else {
+            $('.btn-up').slideUp(300);
+        }
+    });
 })
