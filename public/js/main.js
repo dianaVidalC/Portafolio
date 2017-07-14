1b7bdcd50691e4ffca65ff9696828e77d0b4adca
Erika Vidal <diann852@gmail.com>
2017-07-14 17:33:43 -0500

    scroll

--- public/js/main.js
+++ public/js/main.js
@@ -6,13 +6,20 @@ $(_=>{
     $(".dropdown-button").dropdown({
         belowOrigin: true
     });
+    /*alto de banner*/
     $('.inicio').css({"height": $(window).height() + 'px'});
 
     $(".button-collapse").sideNav();
-    $('.animate').animate()
+
+    /*animacion logo*/
+
+    /* descarga de cv*/
+
     $('.descarga').on('click',_=>{
         document.location="https://drive.google.com/file/d/0B8DCWyMbksMoMEwwU1FSTDRaTkxpWHNkWW90S1NjLWVGSVcw/view?usp=sharing";
     })
+
+    /*boton up*/
 
     $('.btn-up').click(function(){
         $('body, html').animate({
@@ -20,11 +27,31 @@ $(_=>{
         }, 300);
     });
 
-    $(window).scroll(function(){
-        if( $(this).scrollTop() > 0 ){
+    $(window).scroll(_=>{
+        console.log($(this).scrollTop());
+        if( $(this).scrollTop() > 100 ){
             $('.btn-up').slideDown(300);
+            if($(this).scrollTop()>=383){
+                $('.logo-up').addClass("open");
+            }
         } else {
             $('.btn-up').slideUp(300);
         }
     });
+    $('.target').pushpin({
+        top: 0,
+        bottom: 1000,
+        offset: 0
+    });
 })
+
+/*pushpin*/
+
+$('.pushpin-demo-nav').each(function() {
+    var $this = $(this);
+    var $target = $('#' + $(this).attr('data-target'));
+    $this.pushpin({
+        top: $target.offset().top,
+        bottom: $target.offset().top + $target.outerHeight() - $this.height()
+    });
+});
